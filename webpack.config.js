c73965425f7c83e6d45308aadaeed36941e9101d
A-Kimpo <ak.kimpo@gmail.com>
2023-04-11 10:17:36 +0500

    Remove extra comments

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,6 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -41,9 +38,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
